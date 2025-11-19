81d16dc1a2e28d7dae7d7d6d0ff46d506fbcb179
AleGod125 <147008319+AleGod125@users.noreply.github.com>
2025-11-18 19:15:27 -0500

    Supabase

--- bd.py
+++ bd.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:tu_password@localhost:5432/tu_db"
+DATABASE_URL = "postgresql://postgres:1042243376@db.xessdbclpqlupdkrtvvt.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
