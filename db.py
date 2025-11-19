c59abdf1e29b31fa80e35a61559765fa78537b1a
AleGod125 <147008319+AleGod125@users.noreply.github.com>
2025-11-18 21:43:20 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1042243376@db.xessdbclpqlupdkrtvvt.supabase.co:6543/postgres"
+DATABASE_URL = "postgresql://postgres:1042243376@db.xessdbclpqlupdkrtvvt.supabase.co:5432/postgres"
 
 engine = create_engine(
     DATABASE_URL,
