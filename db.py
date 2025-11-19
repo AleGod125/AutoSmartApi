8e477c60cea12132ccd80bda2d3149b6a8e8e5d8
AleGod125 <147008319+AleGod125@users.noreply.github.com>
2025-11-18 21:24:33 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,13 @@
-# db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1042243376@db.xessdbclpqlupdkrtvvt.supabase.co:5432/postgres"
-
+DATABASE_URL = "postgresql+psycopg2://postgres:1042243376@db.xessdbclpqlupdkrtvvt.supabase.co:6543/postgres"
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"sslmode": "require"}
 )
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
